d77d00cd6a6481843f12cd2259258dc591da2c23
Furkan Kartal <Furkan.kartal@testinium.com>
2026-06-23 16:20:28 +0300

    deneme

---
+++ src/test/java/com/testinium/playwrightjavaproject/FirstTest.java
@@ -0,0 +1,40 @@
+package com.testinium.playwrightjavaproject;
+
+import com.microsoft.playwright.*;
+import org.junit.jupiter.api.*;
+
+public class FirstTest {
+    static Playwright playwright;
+    static Browser browser;
+    BrowserContext context;
+    Page page;
+
+    @BeforeAll
+    static void launchBrowser() {
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+    }
+
+    @BeforeEach
+    void createContext() {
+        context = browser.newContext();
+        page = context.newPage();
+    }
+
+    @Test
+    void shouldOpenGoogle() {
+        page.navigate("https://www.google.com");
+        Assertions.assertTrue(page.title().contains("Google"));
+    }
+
+    @AfterEach
+    void closeContext() {
+        context.close();
+    }
+
+    @AfterAll
+    static void closeBrowser() {
+        browser.close();
+        playwright.close();
+    }
+}
