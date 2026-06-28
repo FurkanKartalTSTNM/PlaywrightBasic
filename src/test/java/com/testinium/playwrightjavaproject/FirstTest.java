5786c1513509af3fefe090a88f5a9d5ad0a4f988
Furkan Kartal <Furkan.kartal@testinium.com>
2026-06-28 18:36:41 +0300

    deneme

--- src/test/java/com/testinium/playwrightjavaproject/FirstTest.java
+++ src/test/java/com/testinium/playwrightjavaproject/FirstTest.java
@@ -12,7 +12,7 @@ public class FirstTest {
     @BeforeAll
     static void launchBrowser() {
         playwright = Playwright.create();
-        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(true));
     }
 
     @BeforeEach
