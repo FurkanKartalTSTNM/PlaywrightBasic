1a802a69dc74157bf6f3b598aff6dae6f8c7dc1b
FurkanKartalTSTNM <151616913+FurkanKartalTSTNM@users.noreply.github.com>
2026-09-08 10:28:59 +0300

    Update FirstTest.java

--- src/test/java/com/testinium/playwrightjavaproject/FirstTest.java
+++ src/test/java/com/testinium/playwrightjavaproject/FirstTest.java
@@ -26,7 +26,6 @@ class FirstTest {
         browser = playwright.webkit().launch(
                 new BrowserType.LaunchOptions()
                         .setHeadless(true)
-            demooo
         );
     }
 
